aead10004a6f51a7e9decdad8cd82d939f0d8eb4
GabrielSLange <111287462+GabrielSLange@users.noreply.github.com>
2024-08-24 15:49:57 -0300

    Civitas Shared

---
+++ imobiliariaCivitas_shared/tb_imovel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace imobiliariaCivitas_shared
+{
+    public class tb_imovel
+    {
+        [Key]
+        public int cd_imovel { get; set; }
+        public string descricao { get; set; } = null!;
+        public DateTime criadoEm { get; set; } = DateTime.Now;
+    }
+}
