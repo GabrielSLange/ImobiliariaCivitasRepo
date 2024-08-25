a19b642b2433b382189978a667d397036a2c91ab
GabrielSLange <gabriellange845@gmail.com>
2024-08-24 21:52:53 -0300

    Adicionando Wasm, Blazor Server ao projeto

--- imobiliariaCivitas_pages/obj/Release/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
+++ imobiliariaCivitas_pages/obj/Release/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774f98d1da158e994b2b582911f2d89132e624e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23318ec35750fe510ae95d034842a0125f4ca6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
