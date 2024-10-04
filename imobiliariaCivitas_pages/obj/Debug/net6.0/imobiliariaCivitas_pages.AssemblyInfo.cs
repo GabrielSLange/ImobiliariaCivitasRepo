17bd2f6398cf859016179c8f4024b1400117c341
GabrielSLange <gabriellange845@gmail.com>
2024-10-03 23:47:17 -0300

    Correção do tamanho do arquivo de imagem

--- imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
+++ imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c974403f72a818c0c056843d934956d9d65c809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd96842e51c41a7f04c57dba73e13a27ea7858a")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
