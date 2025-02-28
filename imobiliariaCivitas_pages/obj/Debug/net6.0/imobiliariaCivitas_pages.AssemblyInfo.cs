c68ffdc177a27e0a22c83e337e22f55a41c303a5
GabrielSLange <gabriellange845@gmail.com>
2025-02-28 20:32:18 -0300

    pipipipopopop

--- imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
+++ imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbcd1781f3a2539364fdfd9a4868287f4041de5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529b0df65cac0011c691a55c4270afef557bd505")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
