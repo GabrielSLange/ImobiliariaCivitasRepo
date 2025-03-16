6292f12bb75b2cda20f8ccb8e8e53dc75624be2a
Gabriel Lange - Caiapo <gabriel.lange@caiapo.com.br>
2025-03-16 13:06:55 -0300

    Corrigindo NavigateTo

--- imobiliariaCivitas_shared/obj/Debug/net6.0/imobiliariaCivitas_shared.AssemblyInfo.cs
+++ imobiliariaCivitas_shared/obj/Debug/net6.0/imobiliariaCivitas_shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b2322df7dc42bd289b5e2fdebbd9e7e20720e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22de5d7135faf3cff1f5553a6791dd5a0df99f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
