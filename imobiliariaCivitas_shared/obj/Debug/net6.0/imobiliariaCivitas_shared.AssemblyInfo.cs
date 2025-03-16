265ae9649f1f8129da8fcf0ab83a4194580f7842
Gabriel Lange - Caiapo <gabriel.lange@caiapo.com.br>
2025-03-16 12:51:13 -0300

    Republicando app

--- imobiliariaCivitas_shared/obj/Debug/net6.0/imobiliariaCivitas_shared.AssemblyInfo.cs
+++ imobiliariaCivitas_shared/obj/Debug/net6.0/imobiliariaCivitas_shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd68a31a2be754dd9a41592019dfd56ce806872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b2322df7dc42bd289b5e2fdebbd9e7e20720e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
