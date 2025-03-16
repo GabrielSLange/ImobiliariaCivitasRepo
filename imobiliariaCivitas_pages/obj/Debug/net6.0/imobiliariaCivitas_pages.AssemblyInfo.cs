7bd68a31a2be754dd9a41592019dfd56ce806872
Gabriel Lange - Caiapo <gabriel.lange@caiapo.com.br>
2025-03-16 12:35:45 -0300

    Mudando a estrutura de pastas das Pages do Blazor

--- imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
+++ imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1726611e1aa1a93a93d91e0919bbf024d3a1275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93f875ad94634231965a27b1169c8c5955dbda9")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
