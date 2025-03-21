670a435dada78a918df58c5a99c3dceb8b6a879f
Gabriel Lange - Caiapo <gabriel.lange@caiapo.com.br>
2025-03-21 10:44:15 -0300

    Tentando corrigir o wwroot

--- imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
+++ imobiliariaCivitas_pages/obj/Debug/net6.0/imobiliariaCivitas_pages.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c3f164894f3877c4a316b007816f09102a0e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a340cd5669d6c6f8a60a1c0f345e4be7066d5070")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_pages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
