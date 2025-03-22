fd38535c90a3c50a07c9c43ea8bbc5580b4d3382
Gabriel Lange - Caiapo <gabriel.lange@caiapo.com.br>
2025-03-22 12:21:52 -0300

    sla

--- imobiliariaCivitas_api/obj/Debug/net6.0/imobiliariaCivitas_api.AssemblyInfo.cs
+++ imobiliariaCivitas_api/obj/Debug/net6.0/imobiliariaCivitas_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("imobiliariaCivitas_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f214520b11e2938d891f3d3f7afb5a24984c71d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1796994d97a809df310e3110427df6a50a1a08ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("imobiliariaCivitas_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("imobiliariaCivitas_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
